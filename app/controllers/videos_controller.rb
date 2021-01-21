8d479ef9946061ceed4c821dfc25ee50642b1519
Hanh Solo <minhhanhnyc@gmail.com>
2021-01-20 17:48:27 -0800

    Create method to add video in video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,6 +21,23 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render(
+        status: :ok,
+        # json: video.as_json,
+        # only: [:title, :overview, :release_date, :inventory, :image_url]
+      )
+    else
+      render(
+        status: :bad_request,
+        errors: video.errors
+      )
+    end
+
+  end
+
   private
 
   def require_video
@@ -28,5 +45,9 @@ class VideosController < ApplicationController
     unless @video
       render status: :not_found, json: { errors: { title: ["No video with title #{params["title"]}"] } }
     end
+  end
+
+  def video_params
+    params.permit(:title, :overview, :release_date, :inventory, :image_url, :external_id)
   end
 end
